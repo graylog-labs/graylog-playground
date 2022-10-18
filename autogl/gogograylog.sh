b4b7c2b8e79503103b2dbaa40b0d4c7e3ea45bb5
mcdowellster <95304554+mcdowellster@users.noreply.github.com>
2022-10-18 16:21:02 -0400

    Colours added

--- autogl/gogograylog.sh
+++ autogl/gogograylog.sh
@@ -3,6 +3,12 @@
 # Recommed 16GB of RAM and at least 2 cpus but it CAN run on less
 clear
 
+NC='\033[0m'
+URED='\033[4;31m'
+UGREEN='\033[4;32m'
+UYELLOW='\033[4;33m'
+BGREEN='\033[1;32m' 
+
 if [ "$EUID" -ne 0 ]
   then echo "This script needs to run as root. Please sudo su before running"
   exit
@@ -45,13 +51,22 @@ fi
 #Give some nice details
 echo -e "System Information\nTotal Memory: $TOTAL_MEM\nRAM given to Graylog: $HALF_MEM\nSystem Architecture: $ARCH\nInteral IP: $INTERNAL_IP\nExternal IP: $EXTERNAL_IP\nLogfile: $LOG_FILE\n"
 
+if [ $TOTAL_MEM -lt 8000 ]; then 
+    echo -e "Graylog running on a single node will perform much better with ${URED}8GB${NC} or more system memory"
+    echo -e "Graylog will use ${URED}$HALF_MEM${NC}MB of your total: ${URED}$TOTAL_MEM${NC}MB"
+    echo -e "${UYELLOW}Performance might be impacted...${NC}"
+else
+    echo -e "There is more then 8GB of RAM on this host! Excellent. We will be using HALF of system RAM"
+    echo -e "Graylog will use ${UGREEN}$HALF_MEM${NC}MB of your total: ${UGREEN}$TOTAL_MEM${NC}MB system RAM"
+fi
+
 if [ "$ARCH" != "x86_64" ] && [ "$ARCH" != "aarch64" ]; then
-	echo "Graylog is only supported on x86_64 systems. You are running $ARCH"
+	echo "${URED}Graylog is only supported on x86_64 systems. You are running $ARCH${NC}"
 	exit 64
 fi
 
 function updateSystem {
-	echo "Updating System..."
+	echo "${UGREEN}Updating System...${NC}"
 	if [ "$APT_IS_PRESENT" ]; then
 		apt-get update &>> "$LOG_FILE"
 		apt-get upgrade -y &>> "$LOG_FILE"
@@ -61,10 +76,10 @@ function updateSystem {
 }
 
 if [ "$DOCKER_IS_INSTALLED" ]; then
-    echo -e "Warning! Docker is currently installed. \nThis Script will REMOVE current docker installs and replace with the latest version. \nDo you want to continue? \n[Y/N]"
-    read -s CHOICE
+    echo -e "${UYELLOW}Warning!${NC} Docker is currently installed. \nThis Script will ${URED}REMOVE${NC} current docker installs and replace with the latest version. \nDo you want to continue? \n[Y/N]"
+    read CHOICE
     if [[ $CHOICE != @(y|Y|yes|YES|Yes) ]]; then
-        echo -e "I got an input of $CHOICE so I'm assuming that's a no. Exiting!"
+        echo -e "I got an input of ${URED}$CHOICE${NC} so I'm assuming that's a no. Exiting!"
         exit 1
     fi
 fi
@@ -83,13 +98,13 @@ elif [ "$NFT_IS_PRESENT" ]; then FIREWALL=nft; fi;
 if [ "$APT_IS_PRESENT" ]; then
 	export DEBIAN_FRONTEND=noninteractive
     if [ "$DOCKER_IS_INSTALLED" ]; then
-        echo "Removing current docker install"
+        echo "${URED}Removing current docker install${NC}"
         sudo apt-get remove docker docker-engine docker.io containerd runc &>> "$LOG_FILE"
     fi
     updateSystem
 
     apt-get install -y ca-certificates curl gnupg lsb-release wget &>> "$LOG_FILE"
-    echo -e "Installing Docker"
+    echo -e "${UGREEN}Installing Docker${NC}"
     mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/$ID/gpg | gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
     echo -e "\n\ndeb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$ID $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -98,55 +113,55 @@ if [ "$APT_IS_PRESENT" ]; then
  
 elif [ "$YUM_IS_PRESENT" ]; then
 	    if [ "$DOCKER_IS_INSTALLED" ]; then
-        echo -e "Removing current docker install"
+        echo -e "${URED}Removing current docker install${NC}"
         yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine podman runc  &>> "$LOG_FILE"
     fi
     updateSystem
-    echo -e "Installing Docker" 
+    echo -e "${UGREEN}Installing Docker${NC}" 
     yum install -y yum-utils &>> "$LOG_FILE"
     yum-config-manager --add-repo https://download.docker.com/linux/$ID/docker-ce.repo &>> "$LOG_FILE"
     yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin wget &>> "$LOG_FILE"
 
 else
-	echo -e "This system doesn't appear to be supported. No supported package manager (apt/yum/pacman) was found."
-	echo -e "Automated installation is only availble for Debian and Red-Hat based distrubitions, including Ubuntu and CentOS."
-	echo -e "$NAME is not a supported distribution at this time."
+	echo -e "${URED}This system doesn't appear to be supported. No supported package manager ${UGREEN}(apt/yum)${URED} was found."
+	echo -e "Automated installation is only availble for Debian and Red-Hat based distrubitions, including ${UGREEN}Ubuntu${URED} and ${UGREEN}CentOS${URED}."
+	echo -e "${UGREEN}$NAME${URED} is not a supported distribution at this time.${NC}"
 	exit
 fi
 
 wget https://raw.githubusercontent.com/graylog-labs/graylog-playground/main/autogl/docker-compose.yml -P ~/ &>> "$LOG_FILE"
 if [ ! -f ~/docker-compose.yml ]; then
-    echo -e "Failed to grab docker compose file from GIT... Check your internet connection and try again"
+    echo -e "${URED}Failed to grab docker compose file from GIT... Check your internet connection and try again${NC}"
     exit 1337
 fi
 
 function addFirewallRule {
 	echo "Adding firewall rule for port $1 ($2) via $FIREWALL..."
 	case "$FIREWALL" in
-		none) echo "No firewall installed, please add port $1 manually to your inbound firewall" ;;
+		none) echo "${URED}No firewall installed, please add port $1 manually to your inbound firewall${NC}" ;;
 		ufw) ufw allow from any to any port "$1" proto tcp comment "$2" ;;
 		firewalld) firewall-cmd "--add-port=$1/tcp" --permanent && firewall-cmd --reload ;;
 		iptables) iptables -A INPUT -p tcp -m tcp --dport "$1" -j ACCEPT -m comment --comment "$2" && iptables-save > /etc/iptables/rules.v4 ;;
 		nft) nft add rule filter INPUT tcp dport "$1" accept comment "\"$2\"" ;;
-		*) echo "Unsupported Firewall!" ;;
+		*) echo "${URED}Unsupported Firewall!${NC}" ;;
 	esac
 }
 
 if [[ "$FIREWALL" != "none" ]]; then
-    echo -e "Adding Firewall Rules for Graylog Service Ports and Inputs"
+    echo -e "${UGREEN}Adding Firewall Rules for Graylog Service Ports and Inputsc"
     addFirewallRule "9000" "Default GUI"
     addFirewallRule "5044" "Beats TCP Input"
     addFirewallRule "12201" "GELF TCP Input"
     addFirewallRule "9000" "Default GUI"
 fi
 
-echo -e "Updating Memory Configurations to match system"
+echo -e "${UGREEN}Updating Memory Configurations to match system${NC}"
 sed -i "s+Xms2g+Xms$Q_RAM\m+g" ~/docker-compose.yml
 sed -i "s+Xmx2g+Xmx$Q_RAM\m+g" ~/docker-compose.yml
 sed -i "/GRAYLOG_MONGODB_URI/a\      \GRAYLOG_SERVER_JAVA_OPTS: \"-Xms$Q_RAM\m -Xmx$Q_RAM\m -XX:NewRatio=1 -server -XX:+ResizeTLAB -XX:-OmitStackTraceInFastThrow -Djdk.tls.acknowledgeCloseNotify=true -Dlog4j2.formatMsgNoLookups=true\"" ~/docker-compose.yml
 #
 
-echo -e "Starting up Docker Containers"
+echo -e "${UGREEN}Starting up Docker Containers${NC}"
 docker compose -f ~/docker-compose.yml pull -q &>> "$LOG_FILE"
 docker compose -f ~/docker-compose.yml create &>> "$LOG_FILE"
 docker compose -f ~/docker-compose.yml up -d
@@ -155,17 +170,19 @@ count=0
 while ! curl -s -u 'admin:yabba dabba doo' http://localhost:9000/api/system/cluster/nodes &>> "$LOG_FILE"; do
 	((count++))
     if [ "$count" -eq "30" ]; then
-        echo "Welp. Something went terribly wrong. Check the log file: $LOG_FILE. I'm giving up now! Byeeeeee"
+        echo "${URED}Welp. Something went terribly wrong. Check the log file: $LOG_FILE. I'm giving up now! Byeeeeee${NC}"
         exit 1
     else
-        echo -e "Waiting for graylog to come online"
+        echo -e "${BGREEN}Waiting for graylog to come online${NC}"
         sleep 10s
     fi   
 done
 
 clear
-echo -e "Your Graylog Instance is up and running! Acceess it here: http://$INTERNAL_IP:9000\nIf external access it here: http://$EXTERNAL_IP:9000"
-echo -e "Default user: admin"
-echo -e "Password: yabba dabba doo"
-
+echo -e "${BGREEN}Your Graylog Instance is up and running\nAcceess it here: ${UYELLOW}http://$INTERNAL_IP:9000${BGREEN}\nIf external access it here: ${UYELLOW}http://$EXTERNAL_IP:9000${NC}"
+echo -e "${BGREEN}Default user: ${UYELLOW}admin${NC}"
+echo -e "${BGREEN}Password: ${UYELLOW}yabba dabba doo${NC}"
+echo -e "Docker Compose file is located: ${UYELLOW}$(ls ~/docker-compose.yml)${NC}"
+echo -e "Make changes as needed to open more ports for inputs!"
+echo -e "To make changes, edit the compose file and run:\n${UYELLOW}docker compose -f ~/docker-compose.yml up -d${NC}"
 
