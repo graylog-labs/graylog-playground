49b234c3dff74f46ca690db6a35fa303fcc24077
BlueTeamNinja <1891183+BlueTeamNinja@users.noreply.github.com>
2022-10-20 12:20:56 -0400

    My turn to blow things up

--- autogl/gogograylog.sh
+++ autogl/gogograylog.sh
@@ -52,7 +52,7 @@ fi
 if [ "$CURL_IS_PRESENT" ]; then
 	EXTERNAL_IP=$(curl https://ipecho.net/plain -k 2> /dev/null)
 else
-	EXTERNAL_IP=$(wget -qO- https://ipecho.net/plain 2> /dev/null)
+	EXTERNAL_IP=$(wget -qO- https://ipecho.net/plain --no-check-certificate 2> /dev/null)
 fi
 
 #Give some nice details
